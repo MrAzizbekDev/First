22a5d2143f9e275abea5a003df6d123ef9b4b0dd
azizbek2001 <90821563+azizbek2001@users.noreply.github.com>
2021-09-16 10:52:24 -0700

    Update kridit servis.cpp

--- kridit servis.cpp
+++ kridit servis.cpp
@@ -18,46 +18,5 @@ int main()
     cin>> tel;
     cout<<"hurmatli " <<ism<<" "<<familiya<<" hizmmatlarimizdan foydalanganiz uchun rahmat";
 
-
-
-
-
-
-
-
-
-
-     /*int century;
-    int year;
-    int day;
-
-    int hour;
-    int minute;
-    int second;
-
-
-
-    char flag;
-    cout<<"inter the century: ";
-    cin>>century;
-    year=century*100;
-    cout<<century<<" century is equal to "<<year<<" years\n";
-
-    day=year*365;
-    cout<<year<<" year is equal to "<<day<<" days\n";
-    hour=day*24;
-    cout<<day<<" days is equal to "<<hour<< " hours\n";
-    minute=hour*60;
-    cout<<hour<<" hours is equal to "<<minute<<" minutes\n";
-    second=minute*60;
-    cout<<minute<<" minutes is equal to"<<second<<" seconds\n";
-
-
-
-
-    /*cout <<"enter the time ";
-    cin>>hour>>flag>>minute>>flag>>second;
-    cout<<"seconds:"<<hour*3600+minute*60+second;
-    cout<<"\n minutes: "<<hour*60+minute*1+second*0.6;*/
     return 0;
 }
