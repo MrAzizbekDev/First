75963fcc05cf220b6b0c18f5eeee66d6ab064ec2
AZIZBEK2410 <90616368+AZIZBEK2410@users.noreply.github.com>
2021-09-20 22:07:27 +0500

    Create orta A orta G.cpp

---
+++ orta A orta G.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+   double a,b,x,y;
+   cout<<"a ";
+   cin>>a;
+   cout<<"b ";
+   cin>>b;
+   x=(a+b)/2;
+   //y=a*b;
+   cout<<x<<endl;
+   cout<<sqrt(a*b);
+    return 0;
+}
