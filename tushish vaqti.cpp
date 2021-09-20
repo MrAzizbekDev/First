131744002b0072fc04a0da9eb98ec1ed1e49b2e2
AZIZBEK2410 <90616368+AZIZBEK2410@users.noreply.github.com>
2021-09-20 22:02:38 +0500

    Create tushish vaqti.cpp

---
+++ tushish vaqti.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{int t,h;
+double const  g=9.81;
+  cout<< "balandlik:";
+  cin>>h;
+  t=2*h/g;
+  cout<<sqrt(t);
+    return 0;
+}
