3f71d5f90d807d2efe3264a07ec079f9bee474c3
AZIZBEK2410 <90616368+AZIZBEK2410@users.noreply.github.com>
2021-09-20 22:08:42 +0500

    Create aralashma temp.cpp

---
+++ aralashma temp.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+int main()
+{  string ism;
+    int a,b,c;
+    c=a+b;
+    string familiya;
+    string manzil;
+    int yosh;
+    string tel;
+
+    /*cout<< "Assalomu aleykum "<<" pdp acadey kompaniyasiga hush kelibsiz"<<endl;
+    cout<<"ismingizni kiriting; ";
+    cin>>ism;
+    cout<<"familiya: ";
+    cin>>familiya;
+    cout<<"Manziliz: ";
+    cin>>manzil;*/
+    cout<<"a va b ni kiriting; ";
+    cin>>a>>b;
+    cout<< "c= "<<c;
+    /*cout<<"yoshingiz: ";
+    cin>>yosh;
+    cout<<"telefon raqam: ";
+    cin>> tel;
+    cout<<"hurmatli " <<ism<<" "<<familiya<<" \nhizmmatlarimizdan foydalanganiz uchun rahmat";
+    cout<<"   sizga oq yo'l,biz bn aloqayiz yo'q"*/
+    ;
+
+
+
+
+
+
+
+
+
+
+     /*int century;
+    int year;
+    int day;
+
+    int hour;
+    int minute;
+    int second;
+
+
+
+    char flag;
+    cout<<"inter the century: ";
+    cin>>century;
+    year=century*100;
+    cout<<century<<" century is equal to "<<year<<" years\n";
+
+    day=year*365;
+    cout<<year<<" year is equal to "<<day<<" days\n";
+    hour=day*24;
+    cout<<day<<" days is equal to "<<hour<< " hours\n";
+    minute=hour*60;
+    cout<<hour<<" hours is equal to "<<minute<<" minutes\n";
+    second=minute*60;
+    cout<<minute<<" minutes is equal to"<<second<<" seconds\n";
+
+
+
+
+    /*cout <<"enter the time ";
+    cin>>hour>>flag>>minute>>flag>>second;
+    cout<<"seconds:"<<hour*3600+minute*60+second;
+    cout<<"\n minutes: "<<hour*60+minute*1+second*0.6;*/
+    return 0;
+}
