f3d489ce55c96e1d1968a92b184af17df42890f8
AZIZBEK2410 <90616368+AZIZBEK2410@users.noreply.github.com>
2021-09-22 07:57:10 +0500

    Create 3son kattasi.cpp

---
+++ 3son kattasi.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cin>>a>>b>>c;
+    if ((a>b)&&(a>c)&&(c>b)){
+        cout<<b<<c<<a;
+    }
+    else if((c>a)&&(c>b)&&(a>b)){
+    cout<<b<<a<<c;
+    }
+    else if((b>a)&&(b>c)&&(c>a)){
+    cout<<a<<c<<b;
+    }
+    else if((b>a)&&(b>c)&&(a>c)){
+    cout<<c<<a<<b;}
+
+    else if((a>b)&&(a>c)&&(b>c)){
+    cout<<c<<b<<a;}
+
+    else if((c>a)&&(c>b)&&(b>a)){
+    cout<<a<<b<<c;}
+    return 0;
+}
